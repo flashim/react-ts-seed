0755fa68bfb4c07b6e62434c819c58cafb5d6f45
ashim-conscience <ashims@conscienceit.com>
2018-10-25 15:05:28 +0530

    c

--- app/app.tsx
+++ app/app.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const Demo = ({name} : {name: string}) => <div>Hello {name}!</div>;
+const Demo = ({ name }: { name: string }) => <div>Hello {name}!</div>;
 
 ReactDOM.render(
   <Demo name="World" />,
